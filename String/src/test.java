f7bc2bf9c2b2dc0fd7f5e26235ebcb33073dc7e5
chanho <uni070@naver.com>
2023-01-09 23:59:14 +0900

    2023/01/07

--- String/src/test.java
+++ String/src/test.java
@@ -2,23 +2,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-/*
-    10. 가장 짧은 문자거리
-    설명
-    한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
-
-    입력
-    첫 번째 줄에 문자열 s와 문자 t가 주어진다. 문자열과 문자는 소문자로만 주어집니다.
-    문자열의 길이는 100을 넘지 않는다.
-
-    출력
-    첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
- */
 public class test {
     public static void main(String[] args) {
-        int[] n = {1,2,3,4};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n+1];
+        sc.nextLine();
+        for (int i=0; i < n; i++){
+            arr[i] = sc.nextLine();
+        }
 
+        for (String x : solution(arr, n)){
+            System.out.println(x);
+        }
 
+    }
 
+    public static String[] solution(String[] arr, int n) {
+        String[] answer = new String[n+1];
+
+        int i = 0;
+        for (String x : arr) {
+            answer[i] = new StringBuilder(x).reverse().toString();
+            i++;
+        }
+        return answer;
     }
 }
